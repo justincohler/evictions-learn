7721597a03673c7f399759eb67f71fd0ea1a3073
Justin Cohler <justincohler@gmail.com>
2018-04-26 17:59:28 -0500

    add initial structure

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='evictions-learn',
+      url='',
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      packages=find_packages(),
+     )
