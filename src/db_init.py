82957d5330bffa2f9e36f70c620dc6b7f4591381
Justin Cohler <justincohler@gmail.com>
2018-04-27 18:27:49 -0500

    clean db_init runner

--- src/db_init.py
+++ src/db_init.py
@@ -54,25 +54,7 @@ create_table_block_group = """CREATE TABLE evictions.blockgroup
 cur.execute(create_table_block_group)
 
 # INSERT all rows from dump
-insert_stmnt = """INSERT INTO  evictions.blockgroup(
-state, geo_id, year, name, parent_location,population, poverty_rate, pct_renter_occupied,
-median_gross_rent, median_household_income, median_property_value, rent_burden,
-pct_white, pct_af_am, pct_hispanic, pct_am_ind, pct_asian, pct_nh_pi, pct_multiple,
-pct_other, renter_occupied_households, eviction_filings, evictions, eviction_rate,
-eviction_filing_rate, imputed, subbed)
-VALUES (
-    %s, %s, %s, %s, %s,
-    %s, %s, %s, %s, %s,
-    %s, %s, %s, %s, %s,
-    %s, %s, %s, %s, %s,
-    %s, %s, %s, %s, %s,
-    %s, %s
-    );
-"""
-
 with open('C:/Users/Justin Cohler/output-test.csv', 'r') as f:
-    #reader = csv.reader(f)
-    #next(reader)  # Skip the header row.
     copy_sql = """COPY evictions.blockgroup(
     state, geo_id, year, name, parent_location,population, poverty_rate, pct_renter_occupied,
     median_gross_rent, median_household_income, median_property_value, rent_burden,
