56f29565d3deaef94649bc73f81dde3ff5b79ffe
Justin Cohler <justincohler@gmail.com>
2018-04-29 10:03:38 -0500

    Mask

--- src/set_env.sh
+++ src/set_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DB_USER='ev_user'
-export DB_PASSWORD='alenaclairejustin'
-export DB_HOST='evictions.cuvbjnrvbddj.us-east-1.rds.amazonaws.com'
-export DB_PORT='5432'
+export DB_USER=<YOUR USER>
+export DB_PASSWORD=<YOUR PASSWORD>
+export DB_HOST=<YOUR HOST>
+export DB_PORT=<YOUR PASSWORD>
