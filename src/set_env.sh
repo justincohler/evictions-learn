1305609f8b53742b49df1c8d901095db2dd006f0
Claire Herdeman <cherdeman@uchicago.edu>
2018-04-29 13:35:54 -0500

    analysis folder

--- src/set_env.sh
+++ src/set_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DB_USER='ev_user'
-export DB_PASSWORD='alenaclairejustin'
-export DB_HOST='evictions.cuvbjnrvbddj.us-east-1.rds.amazonaws.com'
-export DB_PORT='5432'
+export DB_USER=ev_user
+export DB_PASSWORD=alenaclairejustin
+export DB_HOST=evictions.cuvbjnrvbddj.us-east-1.rds.amazonaws.com
+export DB_PORT=5432
